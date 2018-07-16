aa4f45d0c8d5f02cc3efd5a44f2da78fb31864b2
Alexander Coffin <alex@wolfflow.io>
2018-07-16 14:50:21 -0500

    Added in a working service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,44 @@
-self.addEventListener('fetch', function(event){
-    console.log("Hello World!");
-    console.log(event.request);
+let reviewCache = 'stage1-v2';
+
+
+self.addEventListener('install', function(event){
+    event.waitUntil(
+    caches.open(reviewCache).then(function(cache) {
+        return cache.addAll([
+            '/',
+            'img/',
+            'css/styles.css',
+            'js/main.js'
+        ]);
+        })
+    );
 });
+
+
+//Compare cache to previous version after install and remove
+//Old Cache's
+self.addEventListener('activate', function(event){
+    event.waitUntil(
+        caches.keys().then(function(cacheNames){
+            return Promise.all(
+                cacheNames.filter(function(cacheName){
+                    return cacheName.startsWith('stage1-') && cacheName != reviewCache;
+        }).map(function(cacheName) {
+            return caches.delete(cacheName);
+            })
+        )      
+        })
+    )
+});
+
+self.addEventListener('fetch', function(event){
+    event.respondWith(
+        caches.match(event.request).then(function(response){
+        if(response) return response;
+        return fetch(event.request);
+    })
+    );
+});
+
+
+
