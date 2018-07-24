7ae74eed56d3830cf683a85e3d1a7ab193c90555
Alexander Coffin <alex@wolfflow.io>
2018-07-24 15:22:48 -0500

    Updated Aria roles to comply with all necessary values.  Updated Alt-text on all images to display a proper name.  Did this through creating a new json value for the image titles

--- js/main.js
+++ js/main.js
@@ -161,7 +161,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = restaurant.name;
+  image.alt = restaurant.photoalt;
   li.append(image);
 
   const name = document.createElement('h1');
