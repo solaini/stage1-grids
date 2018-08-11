eebe06eb246db57f8a09db7dc509feb96c3e3679
Alexander Coffin <alex@wolfflow.io>
2018-08-11 10:10:01 -0500

    Modified for Fetch call.  Still have work to get full list and artifacts from server.

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -5,30 +5,20 @@ class DBHelper {
 
   /**
    * Database URL.
-   * Change this to restaurants.json file location on your server.
+   * Changed this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    return 'http://localhost:1337/restaurants';
   }
 
   /**
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
-        callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
-        callback(error, null);
-      }
-    };
-    xhr.send();
+    fetch(`${DBHelper.DATABASE_URL}`)
+        .then(response => response.json())
+        .then(data => callback(null, data))
+        .catch(error => callback(`Returned Error status of ${error}`));
   }
 
   /**
@@ -151,7 +141,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`/img/${restaurant.photograph}.jpg`);
   }
 
   /**
